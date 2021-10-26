b065c8ddf08cfe282cae2fef7ec95631fd7f72bc
HectorLopez1998 <hectorlopezp1998@gmail.com>
2021-10-25 21:25:37 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+function writeCards(names) {
+  let arr = []
+
+  for (let i = 0; i < names.length; i++) {
+    arr.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+
+  }
+  return arr;
+}
+
+function countDown(count) {
+  let i = 0
+  while (i <= count) {
+    console.log(count - i);
+    i++
+  }
+}
+writeCards(["Ada", "Brendan", "Ali"], 1);
+countDown();
+
+
+
+
+
+
+
+
